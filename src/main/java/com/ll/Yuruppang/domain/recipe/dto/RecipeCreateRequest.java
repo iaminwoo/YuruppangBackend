4a3b7f1888476e4d7ed53141a45558f2b865f625
inwoo <dlsdn1108@gmail.com>
2025-06-16 16:59:57 +0900

    dto 에서 길이 제한 제거

--- src/main/java/com/ll/Yuruppang/domain/recipe/dto/RecipeCreateRequest.java
+++ src/main/java/com/ll/Yuruppang/domain/recipe/dto/RecipeCreateRequest.java
@@ -8,7 +8,6 @@ import java.util.List;
 public record RecipeCreateRequest(
         @NotBlank(message = "레시피 이름은 필수입니다.")
         String name,
-        @Size(max = 500, message = "설명은 최대 500자까지 가능합니다.")
         String description,
         @NotNull(message = "생산 수량은 필수입니다.")
         @Min(value = 1, message = "생산 수량은 1 이상이어야 합니다.")
