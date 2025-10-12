cb68a24f1e6643cac84c57174f0e5b15dbc5e996
inwoo <dlsdn1108@gmail.com>
2025-10-12 17:27:34 +0900

    gemini 모델 변경

--- src/main/java/com/ll/Yuruppang/global/openFeign/gemini/GenAIClient.java
+++ src/main/java/com/ll/Yuruppang/global/openFeign/gemini/GenAIClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
-@FeignClient(name = "geminiClient", url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent")
+@FeignClient(name = "geminiClient", url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent")
 public interface GenAIClient {
 
     @PostMapping(consumes = "application/json")
